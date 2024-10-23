e8095cda54acae859865d86d2be0f0750dd8d9cd
AnandTiwari83 <95517953+AnandTiwari83@users.noreply.github.com>
2024-10-23 18:07:07 +0530

    Create test_chat_completions.sh

---
+++ cloud-service-providers/azure/azureml/cli/test_chat_completions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+curl -X 'POST' \
+  '<your-azureml-endpoint>v1/chat/completions' \
+  -H 'accept: application/json' \
+  -H 'azureml-model-deployment: <deployment-name>' \
+  -H 'Authorization: Bearer <your-azureml-endpoint-token>' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "messages": [
+    {
+      "content": "You are a polite and respectful chatbot helping people plan a vacation.",
+      "role": "system"
+    },
+    {
+      "content": "What should I do for a 4 day vacation in Spain?",
+      "role": "user"
+    }
+  ],
+  "model": "meta/llama-3.1-8b-instruct",
+  "max_tokens": 500,
+  "top_p": 1,
+  "n": 1,
+  "stream": false,
+  "stop": "\n",
+  "frequency_penalty": 0.0
+}'
