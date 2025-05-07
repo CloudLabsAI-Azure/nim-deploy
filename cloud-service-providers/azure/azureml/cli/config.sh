caf0ff46236f317c4f31d73597b230dc05d5e41b
shashankms9 <104433795+shashankms9@users.noreply.github.com>
2025-05-07 11:36:12 +0530

    Update config.sh

--- cloud-service-providers/azure/azureml/cli/config.sh
+++ cloud-service-providers/azure/azureml/cli/config.sh
@@ -39,8 +39,8 @@ image_name="<custom-name-for-nim-image>"
 ngc_container="nvcr.io/nim/meta/llama3-8b-instruct:latest"
 
 # Endpoint related information
-endpoint_name="llama-3-1-8b-nim-endpoint-aml-1"
+endpoint_name="llama-3-8b-nim-endpoint-aml-1"
 
 # Deployment related information
-deployment_name="llama3-1-8b-nim-deployment-aml-1"
+deployment_name="llama3-8b-nim-deployment-aml-1"
 instance_type="Standard_NC24ads_A100_v4"
